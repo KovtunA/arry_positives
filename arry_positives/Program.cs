8c08fae890cedbc27269bfb91d930d4cbbfc3b92
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-09-14 13:53:36 +0300

    first commit

---
+++ arry_positives/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arry_positives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
