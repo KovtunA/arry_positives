5a061cb2cdea4d59e6ca5b27bc69c0c803c23b76
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-09-17 17:49:31 +0300

    minor cleanup

--- arry_positives/Program.cs
+++ arry_positives/Program.cs
@@ -6,25 +6,26 @@ namespace arry_positives
     {
         static void Main(string[] args)
         {
-            int result = 0;
             int[] arr = new int[10];
             Console.WriteLine("Random array:\n");
+
+            int postives = 0;
+            foreach (int number in arr)
+            {
+                if (number > 0) { postives++; }
+            }
+
+            Console.WriteLine($"The amount of positive number: {postives}");
+        }
+
+        static void fillArrayWithRandomNumbers(int[] arr) {
             Random rand = new Random();
+
             for (int i = 0; i < arr.Length; i++)
             {
                 arr[i] = rand.Next(1000) - 500;
                 Console.WriteLine(arr[i]);
             }
-
-            foreach (int number in arr)
-            {
-                if (number > 0)
-                {
-                    result++;
-                }
-            }
-            Console.WriteLine($"The amount of positive number: {result}");
-
         }
     }
 }
